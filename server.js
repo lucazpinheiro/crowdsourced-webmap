6ec8bc4b02be417c8e8bc900ce9ea9bf2862acf7
lucazpinheiro <lucascpinheiro10@gmail.com>
2020-03-31 18:16:12 -0300

    setting up for heroku

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const createError = require('http-errors');
 const express = require('express');
 const mongoose = require('mongoose');
