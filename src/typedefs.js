062745a5ee3542e24b78f7c704e445d2e6f34d94
Lucas <lucascpinheiro10@gmail.com>
2021-10-31 22:59:58 -0300

    doc: add typedef file for jsdocs

---
+++ src/typedefs.js
@@ -0,0 +1,17 @@
+// Spatial data type
+
+/**
+ * @typedef CoordinatesType
+ * @property {number} lng
+ * @property {number} lat
+ */
+
+/**
+ * @typedef FeatureType
+ * @property {string} info
+ * @property {Date} date
+ * @property {CoordinatesType|Array<CoordinatesType>} coords
+ * @property {string} type
+ * @property {string} _id
+ */
+
